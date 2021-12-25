4b12021ba0f3db2048c36b070fe72d6346fa5b88
unknown <wchomrak@gmail.com>
2021-12-24 20:09:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navigation from './components/Navigation';
+import Gallery from './components/Gallery';
+import MintPage from './components/MintPage';
+import MainSection from './components/Main-Section';
+import Section2 from './components/Section2';
+import Section3 from './components/Section3';
+import Section4 from './components/Section4';
+import Footer from './components/Footer';
+
+class App extends Component {
+
+render = () => {
+    return (
+      <Router>
+        <div className="App">
+          <Navigation />
+          <Route path='/' exact>
+            <MainSection />
+            <Section2 />
+            <Section3 />
+            <Section4 />
+          </Route>
+          <Route path='/gallery' exact>
+            <Gallery />
+          </Route>
+          <Route path='/mint' exact>
+            <MintPage />
+          </Route>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
